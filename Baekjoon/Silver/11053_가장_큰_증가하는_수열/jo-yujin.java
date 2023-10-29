1f151d9ba532236b005e12e0c6be7f223e408372
yuzzin0121 <ydbwlsjki60@gmail.com>
2023-10-30 01:24:49 +0900

    Create 가장 큰 증가하는 부분 수열 jo-yujin.java

--- Baekjoon/Silver/11053_가장_큰_증가하는_수열/jo-yujin.java
+++ Baekjoon/Silver/11053_가장_큰_증가하는_수열/jo-yujin.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class jo-yujin {
-    public static void main(String[] args) throws IOException {
+public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,23 +15,23 @@ public class jo-yujin {
         int dp[] = new int[N+1];  // 합을 저장하는 배열
         int[] sequence = new int[N+1];
         for(int i = 1; i <= N; i++) {   // 수열 입력받기
-            dp[i] = sequence[i] = Integer.parseInt(st.nextToken());
+        dp[i] = sequence[i] = Integer.parseInt(st.nextToken());
         }
 
         int result = dp[1];
 
         for(int i = 1; i <= N; i++) {
-            for(int j = 1; j < i; j++) {
-                if(sequence[j] < sequence[i]) {  // i보다 이전인 수가 더 작을 경우
-                    dp[i] = Math.max(dp[j]+sequence[i], dp[i]); // 이전 수의 dp값 + 현재 수, 현재 수의 dp값 중 큰 수를 dp에 저장
-                    result = Math.max(result, dp[i]);   // 동시에 dp의 최댓값 구하기
-                }
-            }
+        for(int j = 1; j < i; j++) {
+        if(sequence[j] < sequence[i]) {  // i보다 이전인 수가 더 작을 경우
+        dp[i] = Math.max(dp[j]+sequence[i], dp[i]); // 이전 수의 dp값 + 현재 수, 현재 수의 dp값 중 큰 수를 dp에 저장
+        result = Math.max(result, dp[i]);   // 동시에 dp의 최댓값 구하기
+        }
+        }
         }
 
         bw.write(Integer.toString(result));
         bw.flush();
         br.close();
         bw.close();
-    }
-}
+        }
+        }
